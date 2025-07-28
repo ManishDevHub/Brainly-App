642f728800316eec949380c69e98b565eea2ccce
YourGitHubUsername <ramuk13476@gmail.com>
2025-07-28 23:10:43 +0530

    brainly-setup

---
+++ Backend/src/db.ts
@@ -0,0 +1,9 @@
+import { Model,Schema } from "mongoose";
+
+const UserSchema = new Schema
+({
+    username: {type: String , unique: true, } ,
+    password: String ,
+})
+
+ export const UserModal = new Model(UserSchema ,"User"  )
