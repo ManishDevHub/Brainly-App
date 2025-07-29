5ea98bbda139e8123e3d2fbf1df6471cf79fbcf0
YourGitHubUsername <ramuk13476@gmail.com>
2025-07-30 00:18:00 +0530

    endpoint-completed

--- Backend/src/db.ts
+++ Backend/src/db.ts
@@ -1,9 +1,19 @@
-import { Model,Schema } from "mongoose";
+import mongoose, { model,Schema } from "mongoose";
 
+mongoose.connect("mongodb+srv://ramuk13476:ZyAH1VK00i9LA4Yb@cluster0.2kyl22s.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 const UserSchema = new Schema
 ({
     username: {type: String , unique: true, } ,
     password: String ,
 })
 
- export const UserModal = new Model(UserSchema ,"User"  );
+const ContentSchema = new Schema({
+    title: String,
+    link: String ,
+    tags: [{ type:mongoose.Types.ObjectId , ref:'Tag'}] ,
+    userId: {type:mongoose.Types.ObjectId , ref:'User' , required:true}
+})
+
+export const ContentModal = model("Content" , ContentSchema)
+ export const UserModal =  model("User", UserSchema );
+  
--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -1,23 +1,88 @@
-import express from 'express' ;
-import mongoose from 'mongoose';
-import jwt from 'jsonwebtoken';
+import express from "express";
+import mongoose from "mongoose";
+import jwt from "jsonwebtoken";
+import { ContentModal, UserModal } from "./db";
+import { UserMiddleware } from "./middleware";
+const JWT_PASSWORD = "123435451546";
+
 const app = express();
+app.use(express.json());
 
-app.post("/api/v1/signup" , ( req , res)=>{
+app.post("/api/v1/signup", async (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+  try {
+    await UserModal.create({
+      username: username,
+      password: password,
+    });
+    res.json({
+      message: "User signed up",
+    });
+  } catch (error) {
+    res.status(411).json({ message: "User already exists" });
+  }
+});
+app.post("/api/v1/signin", async (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+  const existingUser = await UserModal.findOne({
+    username,
+    password,
+  });
+  if (existingUser) {
+    const token = jwt.sign(
+      {
+        id: existingUser._id,
+      },
+      JWT_PASSWORD
+    );
+    res.json({
+      token,
+    });
+  } else {
+    res.status(403).json({
+      message: "Incorrect credentials",
+    });
+  }
+});
 
-})
-app.post("/api/v1/content" , ( req , res)=>{
+app.post("/api/v1/content",UserMiddleware,  async (req, res) => {
+  const link = req.body.link;
+  const type = req.body.type;
+  await ContentModal.create({
+    link,
+    type,
+    //@ts-ignore
+    userId: req.userId,
+    tags:[]
+  })
+   res.json({
+    message:"content added"
+  })
+});
+app.get("/api/v1/content",UserMiddleware, (req, res) => {
+    //@ts-ignore
+    const userId = req.userId;
+    const content = ContentModal.find({
+        userId:userId
+    }).populate("userId" , "username")
+    res.json({
+        content
+    })
+});
+app.delete("/api/v1/content", async (req, res) => {
+    const contentId = req.body.contentId;
+    await ContentModal.deleteMany({
+        contentId,
+        //@ts-ignore
+        userId: req.userId
+    })
+    res.json({
+        message: "Content deleted"
+    })
+});
+app.post("/api/v1/brain/share", (req, res) => {});
+app.get("/api/v1/brain/:shareLink", (req, res) => {});
 
-})
-app.get("/api/v1/content" , ( req , res)=>{
-
-})
-app.delete("/api/v1/content" , ( req , res)=>{
-
-})
-app.post("/api/v1/brain/share" , ( req , res)=>{
-
-})
-app.get("/api/v1/brain/:shareLink" , ( req , res)=>  {
-
-})
+app.listen(3000);
